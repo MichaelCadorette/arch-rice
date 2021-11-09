feee5320afca44c4599c19cdc79cb0d4ae3683ed
checcdev <87030228+checcdev@users.noreply.github.com>
2021-11-09 22:22:56 +0000

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,5 @@
 picom --inactive-opacity=1 &
-feh --bg-fill ~/Pictures/1920-1080.png &
+feh --bg-fill ~/.dwm/wallpaper.png &
 #xrandr --output HDMI-0 --mode 1920x1080 --rate 120 &
 #xrandr --output DP-1 --mode 1920x1080 --rate 144 &
 lxsession &
