87e4281f7a921507ca48f7b646edb8154056c80c
MichaelCadorette <87030228+MichaelCadorette@users.noreply.github.com>
2022-04-10 01:19:23 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,14 +13,14 @@ echo "#    INSTALLING DEPENDENCIES    #"
 echo "#################################"
 sudo pacman -S --noconfirm --needed git base-devel xorg xorg-xinit vim ttf-font-awesome noto-fonts picom lxsession feh xautolock
 
-#create cdos install directory 
+#create cdos install directory
 [[ -f ~/.cdos_install ]] || mkdir ~/.cdos_install
 
 #move dwm config files into their appropriate directories
 [[ -f ~/.dwm ]] || mkdir ~/.dwm
 chmod +x autostart.sh ; cp autostart.sh ~/.dwm
 chmod +x dwmbar.sh ; cp dwmbar.sh ~/.dwm
-cp wallpaper.png ~/.dwm
+cp wallpaper.jpg ~/.dwm
 sudo [[ -f /usr/share/xsessions ]] || mkdir /usr/share/xsessions
 sudo cp dwm.desktop /usr/share/xsessions
 
@@ -45,7 +45,7 @@ echo "########################"
 echo "#    INSTALLING DWM    #"
 echo "########################"
 cd ~/.cdos_install
-git clone https://github.com/checcdev/dwm
+git clone https://github.com/MichaelCadorette/dwm
 cd dwm
 sudo make clean install
 
@@ -60,7 +60,7 @@ echo "#######################"
 echo "#    INSTALLING ST    #"
 echo "#######################"
 cd ~/.cdos_install
-git clone https://github.com/checcdev/st
+git clone https://github.com/MichaelCadorette/st
 cd st
 sudo make clean install
 
@@ -69,8 +69,8 @@ echo "##########################"
 echo "#    INSTALLING DMENU    #"
 echo "##########################"
 cd ~/.cdos_install
-git clone https://github.com/checcdev/dmenu
-cd dmenu 
+git clone https://github.com/MichaelCadorette/dmenu
+cd dmenu
 sudo make clean install
 
 #install slock (simple x locker)
@@ -78,7 +78,7 @@ echo "##########################"
 echo "#    INSTALLING SLOCK    #"
 echo "##########################"
 cd ~/.cdos_install
-git clone https://github.com/checcdev/slock
+git clone https://github.com/MichaelCadorette/slock
 cd slock
 sudo make clean install
 
