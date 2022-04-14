f2f0a9c71679eb1a928ebadd798e2980fd22e86c
MichaelCadorette <87030228+MichaelCadorette@users.noreply.github.com>
2022-04-14 22:12:17 +0000

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #xrandr --output HDMI-0 --mode 1920x1080 --rate 120 &
 #xrandr --output DP-1 --mode 1920x1080 --rate 144 &
 picom --inactive-opacity=1 &
-feh --bg-fill ~/.dwm/wallpaper.png &
+feh --bg-fill ~/.dwm/wallpaper.jpg &
 lxsession &
 xautolock -time 15 -locker slock &
 ~/.dwm/dwmbar.sh &
